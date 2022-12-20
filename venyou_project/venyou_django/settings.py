d34e5627762709531631e669ae725b35285f7842
tim villanueva <timvillanueva3@gmail.com>
2022-12-20 15:32:28 -0600

    another

--- venyou_project/venyou_django/settings.py
+++ venyou_project/venyou_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,13 +58,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
-    "http://0.0.0.0"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    '*'
+    "http://localhost:3000"
 ]
 
 REST_FRAMEWORK = {
@@ -106,7 +104,7 @@ DATABASES = {
         'NAME': 'venyou',
         'USER': 'venuser',
         'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'HOST': 'localhost'
     }
 }
 
@@ -145,8 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
