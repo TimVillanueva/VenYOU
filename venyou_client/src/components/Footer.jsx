66aca33e8daf7c8295714b900325c2adf9f44a62
tyler <tylerwade124@gmail.com>
2022-12-20 11:49:21 -0500

    footer changes

--- venyou_client/src/components/Footer.jsx
+++ venyou_client/src/components/Footer.jsx
@@ -8,7 +8,11 @@ export default function Footer () {
     return (
         <div className='footer-container'>
             <div className='footer'>
-                <a href="https://github.com/TimVillanueva/VenYOU">VenYou</a> <a href="https://github.com/jackmviana">Jack</a> <a href ="https://github.com/TimVillanueva">Tim</a> <a href = "https://github.com/tylerwade124">Tyler</a> <a href = "https://github.com/waltho76">Walter</a>
+                <a href="https://github.com/TimVillanueva/VenYOU" target="_blank">VenYou</a> 
+                <a href="https://github.com/jackmviana" target="_blank">Jack</a> 
+                <a href ="https://github.com/TimVillanueva" target="_blank">Tim</a> 
+                <a href = "https://github.com/tylerwade124" target="_blank">Tyler</a> 
+                <a href = "https://github.com/waltho76" target="_blank">Walter</a>
             </div>
         </div>
     )
